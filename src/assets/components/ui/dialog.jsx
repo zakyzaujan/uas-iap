09887fa2ecce31e82dba97c5de626b83be046c7b
coldweaver0 <zakyzaujan@gmail.com>
2025-05-25 17:34:06 +0700

    Menambahkan ui shadcn sebagai reusable component

---
+++ src/assets/components/ui/dialog.jsx
@@ -0,0 +1,117 @@
+import * as React from "react";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { cn } from "@/assets/lib/utils";
+
+function Dialog(props) {
+  return <DialogPrimitive.Root {...props} />;
+}
+
+function DialogTrigger(props) {
+  return <DialogPrimitive.Trigger {...props} />;
+}
+
+function DialogPortal(props) {
+  return <DialogPrimitive.Portal {...props} />;
+}
+
+function DialogClose(props) {
+  return <DialogPrimitive.Close {...props} />;
+}
+
+function DialogOverlay({ className, ...props }) {
+  return (
+    <DialogPrimitive.Overlay
+      className={cn(
+        "fixed inset-0 bg-black/50",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function DialogContent({ className, children, ...props }) {
+  return (
+    <DialogPortal>
+      <DialogOverlay />
+
+      <div className="fixed inset-0 flex items-center justify-center z-50 p-4">
+        <DialogPrimitive.Content
+          className={cn(
+            "w-full max-w-lg bg-white rounded-lg shadow-lg relative",
+            className
+          )}
+          {...props}
+        >
+          {children}
+
+          <DialogPrimitive.Close className="absolute top-4 right-4 p-1 rounded-lg opacity-70 hover:opacity-100 bg-gray-200/50">
+            <X className="h-5 w-5" />
+            <span className="sr-only">Close</span>
+          </DialogPrimitive.Close>
+        </DialogPrimitive.Content>
+      </div>
+    </DialogPortal>
+  );
+}
+
+function DialogHeader({ className, children, ...props }) {
+  return (
+    <div
+      className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+function DialogFooter({ className, children, ...props }) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+function DialogTitle(props) {
+  return (
+    <DialogPrimitive.Title
+      className={cn(
+        "text-lg font-semibold leading-none tracking-tight",
+        props.className
+      )}
+      {...props}
+    />
+  );
+}
+
+function DialogDescription(props) {
+  return (
+    <DialogPrimitive.Description
+      className={cn("text-sm text-muted-foreground", props.className)}
+      {...props}
+    />
+  );
+}
+
+export {
+  Dialog,
+  DialogTrigger,
+  DialogPortal,
+  DialogOverlay,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+};
